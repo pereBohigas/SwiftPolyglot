69b1a65d279c9ef818d712cfa892d5614808a18f
Pere Bohigas <pere.bohigas@gmail.com>
2024-03-20 22:34:00 +0100

    Refactor missing translations collection

--- Sources/SwiftPolyglotCore/SwiftPolyglot.swift
+++ Sources/SwiftPolyglotCore/SwiftPolyglot.swift
@@ -7,87 +7,137 @@ public struct SwiftPolyglot {
         self.arguments = arguments
     }
 
+    enum Main {
+        enum PolyglotError: Error {
+            case directoryIsEmpty(directoryPath: String)
+            case fileCouldNotBeProcessed(filePath: String)
+        }
+
+        struct MissingTranslation {
+            let filePath: String
+            let originalString: String
+            let missingLanguages: [String]
+
+            var description: [String] {
+                missingLanguages.map { missingLanguage in
+                    "\"\(originalString)\" is missing or not translated in \(missingLanguage) in file \"\(filePath)\""
+                }
+            }
+        }
+
+        static func getMissingTranslationsForFile(_ fileURL: URL, for languages: [String]) throws -> [MissingTranslation] {
+            guard
+                let data = try? Data(contentsOf: fileURL),
+                let jsonObject = try? JSONSerialization.jsonObject(with: data),
+                let jsonDict = jsonObject as? [String: Any],
+                let strings = jsonDict["strings"] as? [String: [String: Any]]
+            else {
+                throw PolyglotError.fileCouldNotBeProcessed(filePath: fileURL.path)
+            }
+
+            let missingTranslations: [MissingTranslation] = strings.flatMap { originalString, translations -> [MissingTranslation] in
+                guard
+                    let localizations = translations["localizations"] as? [String: [String: Any]]
+                else {
+                    return [
+                        .init(
+                            filePath: fileURL.path,
+                            originalString: originalString,
+                            missingLanguages: languages
+                        )
+                    ]
+                }
+
+                let missingTranslations = languages.compactMap { language -> MissingTranslation? in
+                    guard
+                        let langDict = localizations[language],
+                        let stringUnit = langDict["stringUnit"] as? [String: Any],
+                        let state = stringUnit["state"] as? String,
+                        state == "translated"
+                    else { return nil }
+
+                    return .init(
+                        filePath: fileURL.path,
+                        originalString: originalString,
+                        missingLanguages: [language]
+                    )
+                }
+
+                return missingTranslations
+            }
+
+            return missingTranslations
+        }
+
+        static func getMissingTranslationsForDirectory(_ dirPath: String, using fileManager: FileManager, for languages: [String]) throws -> [MissingTranslation] {
+            guard
+                let directoryContents = try? fileManager.contentsOfDirectory(atPath: dirPath)
+            else {
+                throw PolyglotError.directoryIsEmpty(directoryPath: dirPath)
+            }
+
+            let stringFiles = directoryContents.filter { $0.hasSuffix(".xcstrings") }
+
+            let stringFilesURL = stringFiles.map { URL(fileURLWithPath: dirPath).appendingPathComponent($0) }
+
+            let missingTranslations: [MissingTranslation] = try stringFilesURL.flatMap {
+                    try Main.getMissingTranslationsForFile($0, for: languages)
+            }
+
+            return missingTranslations
+        }
+    }
+
     public func run() throws {
         guard CommandLine.arguments.count > 1 else {
             print("Usage: script.swift <language codes> [--errorOnMissing]")
             exit(1)
         }
 
-        let isRunningFromGitHubActions = ProcessInfo.processInfo.environment["GITHUB_ACTIONS"] == "true"
-        let languages = CommandLine.arguments[1].split(separator: ",").map(String.init)
-        let errorOnMissing = CommandLine.arguments.contains("--errorOnMissing")
+        let fileManager: FileManager = .default
 
-        let fileManager = FileManager.default
-        let currentDirectoryPath = fileManager.currentDirectoryPath
+        let isRunningFromGitHubActions: Bool = ProcessInfo.processInfo.environment["GITHUB_ACTIONS"] == "true"
 
-        var missingTranslations = false
+        let languages: [String] = CommandLine.arguments[1].split(separator: ",").map(String.init)
+        let errorOnMissing: Bool = CommandLine.arguments.contains("--errorOnMissing")
 
-        func checkTranslations(in fileURL: URL, for languages: [String]) {
-            guard let data = try? Data(contentsOf: fileURL),
-                  let jsonObject = try? JSONSerialization.jsonObject(with: data),
-                  let jsonDict = jsonObject as? [String: Any],
-                  let strings = jsonDict["strings"] as? [String: [String: Any]]
-            else {
-                if isRunningFromGitHubActions {
-                    print("::warning file=\(fileURL.path)::Could not process file at path: \(fileURL.path)")
-                } else {
-                    print("Could not process file at path: \(fileURL.path)")
-                }
-                return
-            }
+        do {
+            let missingTranslations: [Main.MissingTranslation] = try Main.getMissingTranslationsForDirectory(fileManager.currentDirectoryPath, using: fileManager, for: languages)
 
-            for (originalString, translations) in strings {
-                guard let localizations = translations["localizations"] as? [String: [String: Any]] else {
-                    logWarning(file: fileURL.path, message: "'\(originalString)' is not translated in any language in file: \(fileURL.path)")
-                    missingTranslations = true
-                    continue
-                }
-
-                for lang in languages {
-                    if let langDict = localizations[lang],
-                       let stringUnit = langDict["stringUnit"] as? [String: Any],
-                       let state = stringUnit["state"] as? String, state == "translated"
-                    {
-                    } else {
-                        logWarning(file: fileURL.path, message: "'\(originalString)' is missing or not translated in \(lang) in file: \(fileURL.path)")
-                        missingTranslations = true
-                    }
-                }
-            }
-        }
-
-        func searchDirectory(_ dirPath: String) {
-            if let enumerator = fileManager.enumerator(atPath: dirPath) {
-                for case let file as String in enumerator {
-                    if file.hasSuffix(".xcstrings") {
-                        let fileURL = URL(fileURLWithPath: dirPath).appendingPathComponent(file)
-                        checkTranslations(in: fileURL, for: languages)
-                    }
-                }
-            }
-        }
-
-        func logWarning(file: String, message: String) {
-            if isRunningFromGitHubActions {
-                if errorOnMissing {
-                    print("::error file=\(file)::\(message)")
-                } else {
-                    print("::warning file=\(file)::\(message)")
-                }
+            if missingTranslations.isEmpty {
+                print("All translations are present")
+                exit(EXIT_SUCCESS)
             } else {
-                print(message)
+                let logMessages = missingTranslations.map { missingTranslation in
+                    if isRunningFromGitHubActions {
+                        let type: String = errorOnMissing ? "error" : "warning"
+
+                        return missingTranslation.description.map { "::\(type) file=\(missingTranslation.filePath)::" + $0 }
+                    } else {
+                        return missingTranslation.description
+                    }
+                }
+
+                print(logMessages)
+
+                print("Completed with missing translations")
+
+                if errorOnMissing {
+                    exit(EXIT_FAILURE)
+                }
             }
-        }
+        } catch let Main.PolyglotError.directoryIsEmpty(directoryPath) {
+            print("Error: directory \"\(directoryPath)\" is empty")
 
-        searchDirectory(currentDirectoryPath)
+            exit(EXIT_FAILURE)
+        } catch let Main.PolyglotError.fileCouldNotBeProcessed(filePath) {
+            if isRunningFromGitHubActions {
+                print("::error file=\(filePath)::Could not process file at path: \(filePath)")
+            } else {
+                print("Error: file \"\(filePath)\" could not be processed")
+            }
 
-        if missingTranslations, errorOnMissing {
-            print("Error: One or more translations are missing.")
-            exit(1)
-        } else if missingTranslations {
-            print("Completed with missing translations.")
-        } else {
-            print("All translations are present.")
+            exit(EXIT_FAILURE)
         }
     }
 }
